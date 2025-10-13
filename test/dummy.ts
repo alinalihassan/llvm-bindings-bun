6aba9842f734c9bab8c5e44881d5d9a2e974ffb2
Alin Ali Hassan <hassanalinali@gmail.com>
2025-10-13 17:05:07 +0200

    ci: more testing

--- test/dummy.ts
+++ test/dummy.ts
@@ -1,4 +1,7 @@
-import { LLVMContext } from "@/modules/LLVMContext";
+// import { LLVMContext } from "@/modules/LLVMContext";
+
+import { getLibPath } from "@/utils";
 
 // FFI missing symbols are not shown in proper tests, so we'll just create a dummy module to test the FFI.
-new LLVMContext();
+// new LLVMContext();
+console.log(getLibPath("libclang"));
